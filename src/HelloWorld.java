fce3d6f64b121b8f406690a2030eb14558415574
torre <torre@211.250.190.11>
2019-04-11 14:34:30 +0900

    deleted print() method line.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		System.out.print("Hello, Hahaha");
 		System.out.println("Hi,There!!");
 	}
 }
